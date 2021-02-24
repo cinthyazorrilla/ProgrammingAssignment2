56a31d2864dab9c37a136ebb741a9dd095c826b9
cinthyazorrilla <cinzocis@gmail.com>
2021-02-24 13:28:34 -0500

    tried but could not get it work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makecachematrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+xvec <- matrix( c(5, 1, 0,
+                  3,-1, 2,
+                  4, 0,-1), nrow=3, byrow=TRUE)
+
+makecachematrix.object <- makecachematrix(xvec)
+ls(envir = environment(makecachematrix.object$set))
+cachesolve(xvec)#cannot make it work
